7177c079c586bf77ca2c12cceb7c3d8c943a8dcc
Mariana Godoy <mariana@websdone.co.uk>
2017-10-11 21:39:23 +0100

    first commit

---
+++ src/pages/masterlist/signin.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+@Component({
+  selector: 'page-list',
+  templateUrl: 'signin.html'
+})
+export class signinPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    // If we navigated to this page, we will have an item available as a nav param
+  }
+
+
+}
